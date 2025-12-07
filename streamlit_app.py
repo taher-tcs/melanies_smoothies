7c5a2ed539afd3636342272a41cb8b1223f04bb5
taher-tcs <taher.ahmed@tcs.com>
2025-12-07 16:54:08 +0530

    Update streamlit_app.py
    json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 st.title(f"🥤 Customize Your Smoothie! 🥤")
 st.write(
@@ -37,3 +35,8 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
