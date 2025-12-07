6c8db3bf0805ad43555dc5e8c977598aa7c206e1
taher-tcs <taher.ahmed@tcs.com>
2025-12-07 16:13:57 +0530

    Update streamlit_app.py
    Connected changes needed bc to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
+cnx = st.connection("snowfake")
+session = cnx.session()
 st.title(f"🥤 Customize Your Smoothie! 🥤")
 st.write(
   "Choose the fruits you want in your custom Smoothie!"
