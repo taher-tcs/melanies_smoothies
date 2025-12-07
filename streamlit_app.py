5cd0f46f58988cafca5e9fe9cee3844d15c996b1
taher-tcs <taher.ahmed@tcs.com>
2025-12-07 16:55:55 +0530

    Update streamlit_app.py
    adding data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ if ingredient_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
