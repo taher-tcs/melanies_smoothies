ba1e1c21e0f23ff9849706d5d2e915f752fc9f62
taher-tcs <taher.ahmed@tcs.com>
2025-12-07 16:34:39 +0530

    Update streamlit_app.py
    fix code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-cnx = st.connection("snowfake")
-session = cnx.session()
+
 st.title(f"🥤 Customize Your Smoothie! 🥤")
 st.write(
   "Choose the fruits you want in your custom Smoothie!"
@@ -13,10 +12,13 @@ st.write(name_on_order)
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
 ingredient_list = st.multiselect(
     "chose upto 5 ingredients:",my_dataframe, max_selections=5
 )
 
+cnx = st.connection("snowfake")
+session = cnx.session()
 if ingredient_list:
 
     st.write(ingredient_list)
